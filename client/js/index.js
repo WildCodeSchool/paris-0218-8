d85d57b168971bb70723855f3d338a7d6ee05e3d
Smain Kld <smainkld@gmail.com>
2018-04-02 23:03:47 +0200

    Server Client OK with users datas displaying

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,5 @@
-console.log('loaded')
+fetch ('http://localhost:8080/users')
+	.then(response => response.json())
+	.then(users => {
+		console.log(users)
+	})
--- server/index.js
+++ server/index.js
@@ -15,6 +15,15 @@ const users = [
 
 //console.log(users)
 const app = express()
+app.use((request, response, next) => { 
+response.header('Access-Control-Allow-Origin', '*')
+response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+next()
+})
+
+
+
+
 
 app.get('/', (request, response) => {
 	response.send('OK')

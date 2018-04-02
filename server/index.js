242524dd0d9ae800de5a594bcabd46830dc9ddb9
Smain Kld <smainkld@gmail.com>
2018-04-02 20:15:36 +0200

    mocks loaded

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const user1 = require('../mock/users/user1.json')
+const user2 = require('../mock/users/user2.json')
+const user3 = require('../mock/users/user3.json')
+const user4 = require('../mock/users/user4.json')
+const user5 = require('../mock/users/user5.json')
 
+const users = [
+	user1,
+	user2,
+	user3,
+	user4,
+	user5,
+	]
+console.log(users)
 const app = express()
 
 app.get('/', (request, response) => {

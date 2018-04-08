8918017d8a3177c1ca225408c2d77c76879bbab6
Smain Kld <smainkld@gmail.com>
2018-04-08 17:00:28 +0200

    Displaying users datas

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,6 @@
 fetch ('http://localhost:8080/users')
   .then(response => response.json())
   .then(users => {
-  console.log(users)
-	})
+  	const usersElement = document.getElementById('users')
+  	usersElement.innerHTML = JSON.stringify(users)
+  })

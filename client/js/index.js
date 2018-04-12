e629a24a809d2e1cb2a018f7b9cfa27929207197
Smain Kld <smainkld@gmail.com>
2018-04-12 19:38:44 +0200

    change files JS & CSS

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,17 @@
+import { displayUser } from './component/user.js'
+
+console.log('connected')
+
 window.fetch('http://localhost:8080/users')
   .then(response => response.json())
   .then(users => {
-    const usersElement = document.getElementById('users')
-    usersElement.innerHTML = JSON.stringify(users)
+    const usersElement = document.getElementById('profiles')
+    const userElements = users.map(displayUser).join('')
+
+    usersElement.innerHTML = userElements 
+
   })
+
+
+  const profile = document.getElementById("")
+

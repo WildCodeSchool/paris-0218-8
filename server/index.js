79e4a2424775cb8c3202d76bfe827ce4393875c1
Smain Kld <smainkld@gmail.com>
2018-04-17 22:08:35 +0200

    Read file server added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const fs = require('fs')
+const path = require ('path')
 
-const users = require('./usersArray.json')
-console.log(users)
+const user1 = require('./mock/users/user1.json')
+const user2 = require('./mock/users/user2.json')
+const user3 = require('./mock/users/user3.json')
+const user4 = require('./mock/users/user4.json')
+const user5 = require('./mock/users/user5.json')
+
+const users = [
+	user1, 
+	user2,
+	user3,
+	user4,
+	user5,]
 
 const app = express()
 
@@ -20,12 +32,17 @@ app.get('/users', (request, response) => {
 })
 
 app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id)
+	const filename = `user${request.params.id}.json`
+	const filepath = path.join(__dirname, './mock/users/', filename)
+	console.log({ filename, filepath })
+	fs.readFile(filepath, 'utf8', (err, data) => {
+		if (err) {
+			return response.status(404).end('user not found')
+		}
 
-  const user = users.find(user => user.id === id)
-  
-  response.json(user)
-
+		response.header ('Content-type', 'application/json, charset=utf-8')
+		response.end(data)
+	})
 })
 
 app.listen(8080, () => console.log('Listening to 8080 port'))

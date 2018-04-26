8116e34c61cefae12e3c8e3627c4ffb38315f34b
ValeryC <chinvalery@gmail.com>
2018-04-26 17:38:20 +0200

    login realisé, reste à lier les utilisateurs existant

--- server/index.js
+++ server/index.js
@@ -6,38 +6,121 @@ const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 const readdir = util.promisify(fs.readdir)
 
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+
+const secret = 'something unbelievable'
+
 const app = express()
 
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
 app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Origin', request.headers.origin)
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  response.header('Access-Control-Allow-Credentials', 'true') // important
   next()
 })
 
-app.use((request, response, next) => {
-  if (request.method === 'GET') return next()
-  let accumulator = ''
-  request.on('data', data => {
-    accumulator += data
-  })
-  request.on('end', () => {
-    try {
-      request.body = JSON.parse(accumulator)
-      next()
-    } catch (err) {
-      next(err)
-    }
-  })
+
+// Setup session handler
+app.use(session({
+  secret,
+  saveUninitialized: false,
+  resave: true,
+  store: new FileStore({ secret }),
+}))
+
+
+  const filename = `user-${id}.json`
+  const filepath = path.join(__dirname, './mock/users/', filename)
+
+//Users (hard coded here but consider it comes from database)
+const users = [
+      { login: 'bertrand', password: 'azerty123' },
+      { login: 'martine', password: 'rosedamour' }
+    ]
+
+// Logger middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`, { user: req.session.user, cookie: req.headers.cookie })
+  next()
 })
+
+app.get('/', (req, res) => {
+  const user = req.session.user || {}
+
+  res.json(user)
+})
+
+app.post('/sign-in', (req, res, next) => {
+  // does user exists ?
+  const user = users.find(u => req.body.login === u.login)
+
+  // Error handling
+  if (!user) {
+    return res.json({ error: 'User not found' })
+  }
+
+  if (user.password !== req.body.password) {
+    return res.json({ error: 'Wrong password' })
+  }
+
+  // else, set the user into the session
+  req.session.user = user
+
+  res.json(user)
+})
+
+app.get('/sign-out', (req, res, next) => {
+  req.session.user = {}
+
+  res.json('ok')
+})
+
+app.use((err, req, res, next) => {
+  if (err) {
+    res.json({ message: err.message })
+    console.error(err)
+  }
+
+  next(err)
+})
+
+
+
+
+// app.use((request, response, next) => {
+//   if (request.method === 'GET') return next()
+//   let accumulator = ''
+//   request.on('data', data => {
+//     accumulator += data
+//   })
+//   request.on('end', () => {
+//     try {
+//       request.body = JSON.parse(accumulator)
+//       next()
+//     } catch (err) {
+//       next(err)
+//     }
+//   })
+// })
 
 app.use((request, response, next) => {
   console.log(request.url)
   next()
 })
 
+
 app.get('/', (request, response) => {
   response.send('OK')
 })
+
 
 app.post('/signup', (request, response, next) => {
   const id = Math.random().toString(36).slice(2).padEnd(11, '0')

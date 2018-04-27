9bfeb9807518277a9cb3e44435b815620bb50900
Smain Kld <smainkld@gmail.com>
2018-04-27 11:24:51 +0200

    Page profil CSS Bootstrap

--- client/js/component/user.js
+++ client/js/component/user.js
@@ -10,41 +10,38 @@ export const createUserElement = user =>
   <div> `
 
 export const createUserDetailElement = user => `
-  <div class = "row">
-    <div class = "col-12">
-     <a href="https://placeholder.com"><img src="http://via.placeholder.com/1900x400"></a>
+    <div class = "row">
+      <div class = "col-12 backgroundColor">
+      </div>
     </div>
-  </div>
-
-  <div class="row">
-        <div class="col-7 offset-5">
-          <figure class="figure">
-  <img src=${user.photo} class="figure-img img-fluid rounded-circle profilePic" alt="A generic square placeholder image with rounded corners in a figure.">
-  <figcaption class="figure-caption profileName text-center">${user.username}</figcaption>
-</figure>
-        </div>
+  <div class="profileElements">
+    <div class="row">
+      <div class="col-7 offset-5">
+        <figure class="figure">
+          <img src=${user.photo} class="figure-img img-fluid rounded-circle profilePic" alt="A generic square placeholder image with rounded corners in a figure.">
+          <figcaption class="figure-caption profileName text-center">${user.username}</figcaption>
+        </figure>
       </div>
-      <div class="row">
-        <div class="col-3 offset-5">
+    </div>
+    <div class="row">
+      <div class="col-3 offset-6">
         <p class="profileDetail"> <strong>Mail :</strong> <br> ${user.email}</p>
-        </div>
-        </div>
-        <div class="row">
-        <div class="col-3 offset-5">
-      <p class="profileDetail"> <strong>Téléphone :</strong> <br> ${user.phone}</p>
       </div>
+    </div>
+    <div class="row">
+      <div class="col-3 offset-6">
+        <p class="profileDetail"> <strong>Téléphone :</strong> <br> ${user.phone}</p>
       </div>
-      <div class="row">
-        <div class="col-3 offset-5">
-      <p class="profileDetail"> <strong>Description :</strong> <br> ${user.message}</p>
+    </div>
+    <div class="row">
+      <div class="col-3 offset-6">
+        <p class="profileDetail"> <strong>Description :</strong> <br> ${user.message}</p>
       </div>
+    </div>
+    <div class="row">
+      <div class="col-3 offset-6">
+        <button type="button" class="btn btn-primary sendButton">Contacter</button>
       </div>
-      <div class="row">
-        <div class="col-3 offset-5">
-      <button type="button" class="btn btn-primary sendButton">Contacter</button>
-      </div>
-      </div>
-
-  </div> 
+    </div>
   </div>
 `

c0f1a0caf2aba80cab99ab28608132e70a9c991c
Nadia Djellali <djellali.nadia@gmail.com>
2018-05-02 19:17:02 +0200

    formulaire responsive

--- client/js/map.js
+++ client/js/map.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import { createHeadElement } from './component/head.js'
-document.getElementById('head').innerHTML = createHeadElement()
+import { createHeadElement } from '../map.html'
+document.getElementById('map').innerHTML = createHeadElement()
 
 const google = 'google'
 const initMap = () => {

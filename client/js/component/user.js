88dee2344cb2978905f0f117e65266ee814ca578
Smain Kld <smainkld@gmail.com>
2018-05-02 12:05:05 +0200

    profile page responsive

--- client/js/component/user.js
+++ client/js/component/user.js
@@ -11,36 +11,38 @@ export const createUserElement = user =>
 
 export const createUserDetailElement = user => `
     <div class = "row">
-      <div class = "col-12 backgroundColor"></div>
+      <div class = "col-lg-12 backgroundColor"></div>
     </div>
   <div class="profileElements">
     <div class="row">
-      <div class="col-6 offset-5">
+      <div class="col-lg-6 offset-lg-5 col-6 offset-3">
         <figure class="figure">
-          <img src=${user.photo} class="figure-img img-fluid rounded-circle profilePic" alt="A generic square placeholder image with rounded corners in a figure.">
-          <figcaption class="figure-caption profileName text-center">${user.username}</figcaption>
+          <img src=${user.photo} class=" profileDetail figure-img img-fluid rounded-circle img-responsive profilePic" alt="A generic square placeholder image with rounded corners in a figure.">
+          <figcaption class=" profileDetail figure-caption profileName text-center">${user.username}</figcaption>
         </figure>
       </div>
     </div>
+    <div class="profileDetail">
     <div class="row">
-      <div class="col-5 offset-5">
-        <p class="profileDetail"> <strong>Mail :</strong> <br> ${user.email}</p>
+      <div class="col-lg-6 offset-lg-5 col-12">
+        <p> <strong>Mail :</strong> <br> ${user.email}</p>
       </div>
     </div>
     <div class="row">
-      <div class="col-5 offset-5">
-        <p class="profileDetail"> <strong>Téléphone :</strong> <br> ${user.phone}</p>
+      <div class="col-lg-6 offset-lg-5 col-12">
+        <p> <strong>Téléphone :</strong> <br> ${user.phone}</p>
       </div>
     </div>
     <div class="row">
-      <div class="col-5 offset-5">
-        <p class="profileDetail"> <strong>Description :</strong> <br> ${user.message}</p>
+      <div class="col-lg-6 offset-lg-5 col-12">
+        <p> <strong>Description :</strong> <br> ${user.message}</p>
       </div>
     </div>
     <div class="row">
-      <div class="col-3 offset-5">
+      <div class="col-lg-6 offset-lg-5 col-12">
         <button type="button" class="btn btn-primary sendButton">Contacter</button>
       </div>
+    </div>
     </div>
   </div>
   `
